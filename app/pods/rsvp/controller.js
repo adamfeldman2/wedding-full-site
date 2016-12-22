170fa9471eaa76424bb6b947e985bbdcc265823d
Adam Feldman <adam.feldman@zoocasa.com>
2016-12-22 17:33:13 -0500

    Build RSVP functionality

--- app/pods/rsvp/controller.js
+++ app/pods/rsvp/controller.js
@@ -4,6 +4,8 @@ export default Ember.Controller.extend({
   nameValue: '',
   validName: Ember.computed.gte('nameValue.length', 3),
   isDisabledName: Ember.computed.not('validName'),
+ 
+  isDisabledAttending: false,
   willAttend: null,
   numPeople: null,
 
