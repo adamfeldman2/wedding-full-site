825f687956b48c01abef61a6c9c89bcaac80811c
Adam Feldman <adam.feldman@zoocasa.com>
2016-12-23 11:27:17 -0500

    Write RSVP form flow-logic

--- app/pods/rsvp/controller.js
+++ app/pods/rsvp/controller.js
@@ -4,12 +4,23 @@ export default Ember.Controller.extend({
   nameValue: '',
   validName: Ember.computed.gte('nameValue.length', 3),
   isDisabledName: Ember.computed.not('validName'),
- 
-  isDisabledAttending: false,
-  willAttend: null,
-  numPeople: null,
 
-  // actions
+  willAttend: null,
+  notWillAttend: Ember.computed.equal('willAttend', false),
+  numPeople: null,
+  notNumPeople: Ember.computed.equal('numPeople', false),
+
+  guestName: '',
+  guestNameValid: Ember.computed.gte('guestName.length', 3),
+
+  trueAttending1: Ember.computed.and('willAttend','numPeople', 'guestNameValid'),
+
+  trueAttending2: Ember.computed.and('willAttend','notNumPeople'),
+
+  // One of these properties must be true to activate next button
+  isValidAttending: Ember.computed.or('trueAttending1','trueAttending2','notWillAttend'),
+
+  isDisabledAttending: Ember.computed.not('isValidAttending'),
 
 
 
