9cc6aaf4a08e4c74409ede8d49e1784aacad6600
Adam Feldman <adam.feldman@zoocasa.com>
2016-12-28 13:01:51 -0500

    Create sign-in on admin page

--- app/pods/admin/controller.js
+++ app/pods/admin/controller.js
@@ -1,5 +1,22 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  
+  displayTable: true,
+  adminUsername: '',
+  adminPassword: '',
+  adminUsernameTrue: Ember.computed.equal('adminUsername', 'user1'),
+  adminPasswordTrue: Ember.computed.equal('adminPassword', 'robot1990'),
+  allowAccess: Ember.computed.and('adminUsernameTrue', 'adminPasswordTrue'),
+
+  actions: {
+    adminAccess() {
+      if(this.get('allowAccess')) {
+        this.set('displayTable',false);
+        Ember.$('.admin-sign-in').hide();
+      } else {
+        this.set('tryAgain', 'Nope, try again!');
+        Ember.$('.try-again').fadeIn('slow').delay(1500).fadeOut();
+      }
+    }
+  }
 });
