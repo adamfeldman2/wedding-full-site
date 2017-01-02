4a91232c0efd958e7624a61eb2f9d6ee074a8468
Adam Feldman <adam@adamfeldman.io>
2017-01-02 18:02:18 -0500

    Create couple page

--- app/pods/components/countdown-timer/component.js
+++ app/pods/components/countdown-timer/component.js
@@ -12,6 +12,7 @@ export default Ember.Component.extend({
   weddingDate: 'August 27 2017 20:00:00 EST-0500',
 
   timeRemaining: Ember.computed('weddingDate', 'countdownGreeting', function() {
+
   // Variables used for time remaining //
     const remaining = Date.parse(this.get('weddingDate')) - Date.parse(this.get('today'));
     const seconds = Math.floor( (remaining/1000) % 60 );
@@ -31,28 +32,21 @@ export default Ember.Component.extend({
     if (days >= 0) {
       // this.set('countdownGreeting', 'Wedding Countdown:');
       return `${days} Days, ${hours} Hours, ${minutes} Minutes, and ${seconds} Seconds`;
+
     } else {
       // this.set('countdownGreeting', 'Happily Ever After:');
       return `Living happily ever after for ${daysPassed} Days, ${hoursPassed} Hours, ${minutesPassed} Minutes, and ${secondsPassed} Seconds.`;
     }
   }),
 
+
+
+
+
   actions: {
     alertTest() {
       console.log('This works!');
       this.toggleProperty('buttonClicked')
-    },
-
-    timeRemaining() {
-
-      const remaining = Date.parse(this.get('weddingDate')) - Date.parse(new Date);
-
-      const seconds = Math.floor( (remaining/1000) % 60 );
-      const minutes = Math.floor( (remaining/1000/60) % 60 );
-      const hours = Math.floor( (remaining/(1000*60*60)) % 24 );
-      const days = Math.floor( remaining/(1000*60*60*24) );
-
-      this.set('showRemainder',`There are ${days} Days, ${hours} Hours, ${minutes} Minutes, and ${seconds} Seconds left until the wedding!`);
     }
   }
 
