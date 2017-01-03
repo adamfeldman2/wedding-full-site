a9e1307dcf4c91179a593c6d212f4801b27788c1
Adam Feldman <adam.feldman@zoocasa.com>
2017-01-03 14:17:25 -0500

    Add weather to details page

--- app/pods/weather/controller.js
+++
@@ -1,5 +0,0 @@
-import Ember from 'ember';
-
-export default Ember.Controller.extend({
-  city: Ember.computed.alias('model.location.city')
-});
--- app/pods/weather/model.js
+++
@@ -1,7 +0,0 @@
-import DS from 'ember-data';
-
-export default DS.Model.extend({
-  city: DS.attr('string'),
-  tempC: DS.attr('number'),
-  iconUrl: DS.attr('string')
-});
