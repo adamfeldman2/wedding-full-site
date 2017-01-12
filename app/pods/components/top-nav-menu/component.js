2619e3ce469c574d9b6531dd317cf770a4d143e5
Adam Feldman <adam.feldman@zoocasa.com>
2017-01-12 11:38:36 -0500

    Test various ideas

--- app/pods/components/top-nav-menu/component.js
+++ app/pods/components/top-nav-menu/component.js
@@ -1,8 +1,21 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  showHamburger: true,
-  
+  tagName: 'top-nav-menu',
+  showHamburger: false,
+
+  setupClickEventHandler: Ember.on('didInsertElement', function() {
+    Ember.$(document).on('click', this.clickHandlerFunction);
+  }),
+
+  destroyClickEventHandler: Ember.on('didDestroyElement', function() {
+    Ember.$(document).off('click', this.clickHandlerFunction);
+  }),
+
+  clickHandlerFunction(event) {
+    console.log(event.target, Ember.$('top-nav-menu'));
+  },
+
   actions: {
     toggleMenu() {
       this.toggleProperty('showHamburger');
