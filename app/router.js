d8770c9cfcb0e4645c9ec6d2e2e58b0d610ec4c6
Adam Feldman <adam@adamfeldman.io>
2017-02-04 10:57:59 -0500

    Use index as index path

--- app/router.js
+++ app/router.js
@@ -7,8 +7,8 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  // this.route('index', {path: '/'});
-  this.route('home', { path: '/' });
+  this.route('index', {path: '/'});
+  this.route('home');
   this.route('couple');
   this.route('details');
   this.route('rsvp');
