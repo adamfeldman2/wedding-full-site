96b93590a453a748430818802697c5909950a4e8
Adam Feldman <feldman238@gmail.com>
2017-08-27 09:13:16 -0400

    Set target time to midnight of Aug 27th

--- app/pods/components/countdown-timer/component.js
+++ app/pods/components/countdown-timer/component.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   tagName: 'countdown-wrapper',
   classNames: ['fadeIn'],
-  targetTime: new Date("Aug 27, 2017 17:30:00").getTime(),
+  targetTime: new Date("Aug 27, 2017 23:59:00").getTime(),
   currentTime: new Date().getTime(),
   timeRemaining: Ember.computed('targetTime', 'currentTime', function() {
     return this.get('targetTime') - this.get('currentTime');
